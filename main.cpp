af4b265447b01151ccabf7e7d34c06d71e98fd26
hientran-sudo <79175744+hientran-sudo@users.noreply.github.com>
2021-08-27 14:19:37 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+ifstream f ("data.in");
+ofstream g ("data.out");
+int main()
+{
+    cout << "Hello world!" << endl;
+    int a, b, sum;
+    f>>a>>b;
+    sum = a+b;
+    g<<sum;
+
+    return 0;
+}
